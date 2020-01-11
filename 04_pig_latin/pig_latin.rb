3e82bbd25722e697e9d5279a7abe706597747a44
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-01-11 07:24:14 -0500

    learn_ruby pig latin complete

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,30 @@
 #write your code here
+
+$vowels = ['a', 'e', 'i','o', 'u']
+
+def translate input
+    words = input.split
+    wordResultArray = []
+
+    words.each do |word|
+        result = ""
+
+        if $vowels.include?(word.chars.first.downcase)
+            wordResultArray << word + "ay"
+        else
+            charArray = word.chars
+            while !$vowels.include?(charArray.first.downcase)
+                if charArray.first == "q"
+                    charArray = charArray.rotate(1)
+                    if charArray.first == "u"
+                        charArray = charArray.rotate(1)
+                    end
+                else
+                    charArray = charArray.rotate(1)
+                end
+            end
+            wordResultArray << charArray.join + "ay"
+        end
+    end
+    wordResultArray.join(" ")
+end
